90b792ed6feadd546cc252a306fa4f5e70311ae2
singto1597 <singto1597@gmail.com>
2026-05-31 17:36:33 +0700

    add web domain'

--- main.py
+++ main.py
@@ -61,7 +61,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173", 
         "http://127.0.0.1:5173",
-        "https://classtestts.singto1597.xyz"
+        "https://classtestts.singto1597.xyz",
+        "https://classts.singto1597.xyz",
+        "https://class.singto1597.xyz"
     ],
     allow_credentials=True,
     allow_methods=["*"],
